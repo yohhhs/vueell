c044af912c18384ee3c01a4c68ec0a01e3181af5
haohaoshuo <591005835@qq.com>
2017-06-10 22:52:43 +0800

    init

---
+++ src/components/linetxt/linetxt.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="des-title">
+    <div class="line"></div>
+    <div class="text">{{txt}}</div>
+    <div class="line"></div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      txt: {
+        type: String
+      }
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .des-title
+    display: flex
+    width: 80%
+    margin: 28px auto 24px auto
+    .line
+      flex: 1
+      position: relative
+      top: -6px
+      border-bottom: 1px solid rgba(255, 255, 255, .2)
+    .text
+      padding: 0 12px
+      font-weight: 700
+      font-size: 14px
+</style>
